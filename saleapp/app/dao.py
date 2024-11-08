65582fd33b623638c570194ed93df38a9ae8c3e2
Your Name <you@example.com>
2024-11-08 11:25:58 +0700

    Connect Database1

---
+++ saleapp/app/dao.py
@@ -0,0 +1,14 @@
+from app.models import Category, Products
+
+
+def load_categories():
+    return Category.query.order_by('id').all()
+
+
+def load_products(cate_id=None, kw=None):
+    query = Products.query
+    if kw:
+        query = query.filter(Products.name.contains(kw))
+    if cate_id:
+        query = query.filter(Products.category_id == cate_id)
+    return query.all()
